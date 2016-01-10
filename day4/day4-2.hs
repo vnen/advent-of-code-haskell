207f2eaad5acb47c7b655a0880709a6940ff8d79
George Marques <george@gmarqu.es>
2016-01-10 20:04:06 -0200

    Add both solutions for day 4

---
+++ day4/day4-2.hs
@@ -0,0 +1,20 @@
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C
+import qualified Crypto.Hash.MD5 as MD5
+
+main = do
+    input <- return $ C.pack "ckczppom"
+    putStrLn $ show (bestNumber input)
+
+bestNumber :: BS.ByteString -> Int
+bestNumber input =
+    head [x | x <- [1..], check (MD5.hash (concatI input x))]
+
+check :: BS.ByteString -> Bool
+check x =
+    let (a:b:c:xs) = BS.unpack x
+    in a == 0 && b == 0 && c == 0
+
+concatI :: BS.ByteString -> Int -> BS.ByteString
+concatI bs i = C.pack $ (C.unpack bs) ++ (show i)
+

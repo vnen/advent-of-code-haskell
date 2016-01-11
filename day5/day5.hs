dfb9e79d7d35a0e196a0e4cef8b423972f3b7c2f
George Marques <george@gmarqu.es>
2016-01-10 22:18:52 -0200

    Add solution for part 1 of day 5

---
+++ day5/day5.hs
@@ -0,0 +1,22 @@
+import System.IO
+import Data.List
+
+main = do
+    input <- readFile "input5.txt"
+    putStrLn $ show (nices input)
+
+nices :: String -> Int
+nices = length . filter isNice . lines
+
+isNice :: String -> Bool
+isNice line = forbidden line && threeVowels line && doubled line
+
+doubled :: String -> Bool
+doubled [] = False
+doubled (_:[]) = False
+doubled (x:y:[]) = x == y
+doubled (x:y:xs) = (x == y) || (doubled (y:xs))
+
+threeVowels x = 3 <= (length $ [c | c <- x, c `elem` "aieou"])
+forbidden x = not $ any (flip isInfixOf x) ["ab", "cd", "pq", "xy"]
+
